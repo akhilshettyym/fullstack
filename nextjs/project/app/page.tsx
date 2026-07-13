c277a8c8426addd1316c82c69838224d95cf689b
Shetty, Akhil <akhilshettym2003@gmail.com>
2026-07-13 21:57:12 +0530

    nextjs project added

---
+++ nextjs/project/app/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+    
+  return (
+    <div>
+      Hi
+    </div>
+  )
+}
+
+export default page;
---
+++ nextjs/project/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  
+    turbopackFileSystemCacheForBuild: true,
+  },
+};
+
+export default nextConfig;
